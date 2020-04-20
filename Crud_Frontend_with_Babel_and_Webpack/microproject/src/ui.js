8970f168e8b4bbdfa5fdf9948faeabdbcbae2c28
Edyta Godniak (ego01) <edyta.godniak@hrs.com>
2020-04-20 13:04:51 +0200

    add changes in crud app

---
+++ Crud_Frontend_with_Babel_and_Webpack/microproject/src/ui.js
@@ -0,0 +1,119 @@
+class UI {
+
+  constructor() {
+    this.post = document.querySelector('#posts');
+    this.titleInput = document.querySelector('#title');
+    this.bodyInput = document.querySelector('#body');
+    this.idInput = document.querySelector('#id');
+    this.postSubmit = document.querySelector('.post-submit');
+    this.post = document.querySelector('#posts');
+    this.formState = 'add';
+  }
+
+  showPosts(posts) {
+    let output = '';
+    posts.forEach((post) => {
+      output += `
+        <div class="card mb-3">
+          <div class="card-body">
+            <h4 class="card-title">${post.title}</h4>
+            <p class="card-text">${post.body}</p>
+            <a href="#" class="edit card-link" data-id="${post.id}">
+              <i class="fa fa-pencil"></i>
+            </a>
+            <a href="#" class="delete card-link" data-id="${post.id}">
+            <i class="fa fa-remove"></i>
+            </a>
+          </div>
+        </div>
+      `;
+    });
+    this.post.innerHTML = output;
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+    //create div
+    const div = document.createElement('div');
+    //add class
+    div.className = className;
+    //add messaege
+    div.appendChild(document.createTextNode(message));
+    //get parent
+    const container = document.querySelector('.postsContainer');
+    //get posts
+    const posts = document.querySelector('#posts');
+    //insert alert before posts
+    container.insertBefore(div, posts);
+
+    //Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 6000)
+  }
+
+  //clear allert message
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  //clear form
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
+
+  //fill form to edit
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  clearIdInput() {
+    this.idInput.value ="";
+  }
+
+  //change form state
+  changeFormState(type) {
+    if(type == 'edit') {
+      //create button look
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+      
+      if (this.formState !='edit') { //we do not want to create multiple cancell buttons
+        //create cancell button  - we could also have it in index.html and make it visible/hidden
+        const button = document.createElement('button');
+        button.className = 'post-cancel btn btn-light btn-block';
+        button.appendChild(document.createTextNode('Cancel Edit'));
+
+        //get parent
+        const cardForm = document.querySelector('.card-form');
+        //get element to insert before
+        const formEnd = document.querySelector('.form-end');
+        //insert cancel button
+        cardForm.insertBefore(button, formEnd);
+      }
+
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      //remove cancel button if exists
+      if (document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      //clear id from hidden field
+      this.clearIdInput();
+      //clear text
+      this.clearFields();
+    }
+    this.formState = type;
+  }
+}
+
+export const ui = new UI();
